bd67a06edef9a199f5c356439c91da7f95340ff3
ravenclawn <macaroni.ubisoft@gmail.com>
2025-05-10 15:16:56 +0200

    tried bootstrap, linked files to html

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,1 +1,9 @@
 /* Entry Point */
+
+
+const button = document.createElement('button')
+button.type = 'button';
+button.className = 'btn btn-primary';
+button.textContent = "Button!";
+
+document.getElementById('app').appendChild(button);
